2c18fe09db62e62bf1d26524634d70930b1ea28d
ClaytonJosue <jogamesbr123@gmail.com>
2023-10-24 20:28:21 -0300

    create the introduction section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
 import HeaderPage from "@/components/Header";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <HeaderPage />
-      <h1>Hello World!</h1>
+      <main className="p-2">
+        <h2 className="text-center text-2xl mb-5">
+          Déficit de Profissionais na área de TI no Brasil
+        </h2>
+        {/* Introdução */}
+        <section>
+          <p className="leading-7">
+            Sabemos que o mercado na área de TI é enorme e está crescendo cada
+            vez mais, porém, de acordo com o Google, o mercado de tecnologia da
+            informação (TI) no Brasil terá um{" "}
+            <strong>déficit de 530 mil profissionais</strong>.
+          </p>
+          <Image
+            src="/ti_image.jpg"
+            alt="Imagem de um notebook"
+            className="w-full mt-4 rounded-sm"
+            width={200}
+            height={200}
+          />
+          <p className="text-center text-sm text-gray-800">
+            (Imagem meramente ilustrativa)
+          </p>
+        </section>
+      </main>
     </>
   );
 }
